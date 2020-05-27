642328abbc86034204a6367ed5e0a555bae050a0
unknown <you@example.com>
2020-05-27 22:10:12 +0530

    source file added

---
+++ Source code(new)+extra(final)/app/src/main/java/me/varunon9/remotecontrolpc/MainActivity1.java
@@ -0,0 +1,17 @@
+package me.varunon9.remotecontrolpc;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import static android.widget.Toast.LENGTH_LONG;
+
+public class MainActivity1 {
+}
